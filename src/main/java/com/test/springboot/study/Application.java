c9f07714abe7b8c5b9994cc785fe4e0af8a46767
eurekasolution <nadopro@gmail.com>
2021-07-19 14:10:59 +0900

    Step 6 HelloController 만들었다.

--- src/main/java/com/test/springboot/study/Application.java
+++ src/main/java/com/test/springboot/study/Application.java
@@ -1,7 +1,5 @@
 package com.test.springboot.study;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
 Step 5. 프로그램의 시작포인트를 만든다: main()
@@ -9,6 +7,9 @@ Step 5. 프로그램의 시작포인트를 만든다: main()
     스프링부트의 시작설정, 자동 설정
     프로젝트의 최상단에 위치
  */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
---
+++ src/main/java/com/test/springboot/study/web/HelloController.java
@@ -0,0 +1,41 @@
+package com.test.springboot.study.web;
+
+import org.springframework.web.bind.annotation.*;
+
+
+/*
+Step 6. HelloController
+
+    Method : 405 Method Not Allowed
+    GET:  test.com/a.jsp?id=user&pass=1234 ( 256Byte ) HTTP Header , 보안문제
+    POST: 입력창, HTTP Body, 보안 유리
+    PUT,
+    DELETE
+
+    http://tistory.com/user/a.jsp?idx=123
+    http://tistory.com/user/123
+    http://tistory.com/user/hello
+
+    @RestController
+        JSON으로 반환하는 컨트롤러 making
+        JavaScript Object Notation
+        "/hello":"hello"
+
+        스프링 @ResponseBody 대체
+
+    @GetMapping
+        주소창에 오는 정보(get method)를 매핑해 줘
+ */
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping
+    public String test() {
+        return "test";
+    }
+}
