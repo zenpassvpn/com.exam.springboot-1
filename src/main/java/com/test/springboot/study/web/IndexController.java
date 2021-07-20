f5ebc97821ec612e33f73a592152b67eceef7d2e
eurekasolution <nadopro@gmail.com>
2021-07-20 17:07:27 +0900

    Step 31 Mustache template 폴더 이름 변경 ->templates index.mustache 추가

--- src/main/java/com/test/springboot/study/web/IndexController.java
+++ src/main/java/com/test/springboot/study/web/IndexController.java
@@ -1,14 +1,17 @@
 package com.test.springboot.study.web;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    // http://localhost:8080
+
 
     @GetMapping("/")
     public String index() {
         return "index";
     }
+
+
 }
