8bf254662178a9366ddac82e58c35f8292a4515b
eurekasolution <nadopro@gmail.com>
2021-07-20 10:36:09 +0900

    Step 23 PostsRepositoryTest 추가

---
+++ src/test/java/com/test/springboot/study/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.test.springboot.study.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+Step 23 PostsRepositoryTest 추가
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void saveAndLoad() {
+        String title = "테스트 제목";
+        String content = "테스트 본문";
+
+        postsRepository.save( Posts.builder().title(title).content(content).author("user@test.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
