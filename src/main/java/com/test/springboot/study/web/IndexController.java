2be0546138c4d8af9b13124b628da55e9339b297
eurekasolution <nadopro@gmail.com>
2021-07-21 14:45:27 +0900

    Step 31-5 IndexController index.mustache 수정

--- src/main/java/com/test/springboot/study/web/IndexController.java
+++ src/main/java/com/test/springboot/study/web/IndexController.java
@@ -1,12 +1,19 @@
 package com.test.springboot.study.web;
 
 
+import com.test.springboot.study.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/*
+객체로 넘어온 데이터를 템플릿 엔진(mustache)에 넘겨준다.
+ */
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
-
+    private final PostsService postsService;
 
     /*
     Step 31:
@@ -15,7 +22,8 @@ public class IndexController {
      */
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
 
