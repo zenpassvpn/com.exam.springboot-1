d5c46dd6d84031ec055e2400e3d472bc9a4714d5
eurekasolution <nadopro@gmail.com>
2021-07-21 16:01:01 +0900

    Step 32-2 config.auth/SecurityConfig.java

--- src/main/java/com/test/springboot/study/domain/user/UserRepository.java
+++ src/main/java/com/test/springboot/study/domain/user/UserRepository.java
@@ -1,4 +1,9 @@
 package com.test.springboot.study.domain.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
