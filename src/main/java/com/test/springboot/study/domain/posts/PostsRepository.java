f4d8663219611a0d32d26270cb12167fd90644a8
eurekasolution <nadopro@gmail.com>
2021-07-21 10:52:15 +0900

    Step 31-3 Query를 직접 등록하는 방법까지 추가

--- src/main/java/com/test/springboot/study/domain/posts/PostsRepository.java
+++ src/main/java/com/test/springboot/study/domain/posts/PostsRepository.java
@@ -1,7 +1,12 @@
 package com.test.springboot.study.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
+// named sql
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
+    @Query("SELECT p FROM Posts AS p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
